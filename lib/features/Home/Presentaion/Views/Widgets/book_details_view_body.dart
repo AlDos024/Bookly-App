08953a793bed0a1274911d91cfb451cd320d23ca
Al-Dos024 <amjdos0amjdos0190edu@gmail.com>
2024-03-26 09:43:10 +0200

    Done with details screen

--- lib/features/Home/Presentaion/Views/Widgets/book_details_view_body.dart
+++ lib/features/Home/Presentaion/Views/Widgets/book_details_view_body.dart
@@ -1,52 +1,39 @@
-import 'package:bookly_app/core/util/styles.dart';
-import 'package:bookly_app/features/Home/Presentaion/Views/Widgets/book_rating.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'books_details_section.dart';
 import 'custom_book_details_appbar.dart';
-import 'custom_list_view_item.dart';
+import 'similar_books_section.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var tempwidth = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25.0),
-      child: Column(
-        children: [
-          const CustomBookdetailsAppBar(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: tempwidth * .17),
-            child: const CustomBookImg(),
-          ),
-          const SizedBox(
-            height: 43,
-          ),
-          Text(
-            "The Song of Ice & Fire",
-            style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 6,
-          ),
-          Opacity(
-            opacity: 0.7,
-            child: Text(
-              "George R.R Martin",
-              style: Styles.textStyle18.copyWith(
-                  fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+    return const CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25.0),
+            child: Column(
+              children: [
+                CustomBookdetailsAppBar(),
+                BookDetailsSection(),
+                Expanded(
+                  child: SizedBox(
+                    height: 18,
+                  ),
+                ),
+                SimilarBooksSection(),
+                SizedBox(
+                  height: 40,
+                ),
+              ],
             ),
           ),
-          const SizedBox(
-            height: 18,
-          ),
-          const BookRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          )
-        ],
-      ),
+        )
+      ],
     );
   }
 }
---
+++ lib/features/Home/Presentaion/Views/Widgets/books_details_section.dart
@@ -0,0 +1,52 @@
+
+import 'package:bookly_app/core/util/styles.dart';
+import 'package:bookly_app/features/Home/Presentaion/Views/Widgets/custom_action.dart';
+import 'package:flutter/material.dart';
+
+import 'book_rating.dart';
+import 'custom_list_view_item.dart';
+
+class BookDetailsSection extends StatelessWidget {
+  const BookDetailsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var tempwidth = MediaQuery.of(context).size.width;
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: tempwidth * .2),
+          child: const CustomBookImg(),
+        ),
+        const SizedBox(
+          height: 32,
+        ),
+        Text(
+          "The Song of Ice & Fire",
+          style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(
+          height: 6,
+        ),
+        Opacity(
+          opacity: 0.7,
+          child: Text(
+            "George R.R Martin",
+            style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+          ),
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        const BookRating(
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        const CustomAction(),
+      ],
+    );
+  }
+}
